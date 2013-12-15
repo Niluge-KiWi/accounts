f9a20e3b178ad79af533640113106187fd5aceb1
Thomas Riccardi <riccardi.thomas@gmail.com>
2013-12-15 21:25:00 +0100

    Parse payments from file

--- accounts.js
+++ accounts.js
@@ -1,18 +1,32 @@
 #!/usr/bin/env node
 
+var fs = require('fs');
+
 var Accounts = require('./lib/accounts');
 
+var argv = require('optimist')
+      .usage('Usage: $0 -p payments.json')
+      .demand(['p'])
+      .alias('p', 'payments')
+      .describe('p', 'the payments file')
+      .argv;
 
-var payments = [
-  new Accounts.Payment('A', 10, ['A', 'B']),
-  new Accounts.Payment('B', 2, ['A', 'B'])
-];
 
-var account = new Accounts.Account('test account', payments);
+var paymentsJson = JSON.parse(fs.readFileSync(argv.payments));
+
+var payments = [];
+paymentsJson.forEach(function(p) {
+  var payment = new Accounts.Payment(p.buyer, p.sum, p.recipients, p.comment);
+  payments.push(payment);
+});
+
+console.log('payments', payments);
+
+var account = new Accounts.Account('account', payments);
 
 
 
 account.reduce();
 
-console.log(account.dumpDebts());
+console.log('reduced debts', account.dumpDebts());
 
